7d248b45721c8a4e54df8eb5b8376b2045509ef7
alvnfaiz <alvin.faiz.10@gmail.com>
2023-12-03 02:22:51 +0700

    button text color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement> & { classNa
     return (
         <button
             {...props}
-            className={`text-white font-bold py-2 px-4 rounded-lg ${className && className} ${props.disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+            className={`font-bold py-2 px-4 rounded-lg ${className && className} ${props.disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
         >
             {children}
         </button>
