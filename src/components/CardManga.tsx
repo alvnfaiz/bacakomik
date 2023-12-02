22db08a01b58a7c4df30b6b4359997f729d0b6ea
alvnfaiz <alvin.faiz.10@gmail.com>
2023-12-02 19:36:56 +0700

    responsif

--- src/components/CardManga.tsx
+++ src/components/CardManga.tsx
@@ -9,7 +9,7 @@ const CardManga: React.FC<Props> = ({ manga }) => {
     return (
         <Link
             to={`/manga/${manga.source}/${manga.url}`}
-            className="w-56 p-2 overflow-hidden border border-gray-200 rounded-md shadow-sm hover:bg-white hover:shadow-lg hover:border-purple-500 group">
+            className="w-1/2 p-2 overflow-hidden border border-gray-200 rounded-md shadow-sm sm:w-1/3 md:w-56 hover:bg-white hover:shadow-lg hover:border-purple-500 group">
             <div className="relative w-full h-64 overflow-hidden">
                 <img
                     src={manga.thumbnail?manga.thumbnail:`https://placehold.co/200x300/8b5cf6/fff?text=${manga.title}`}
